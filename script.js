45a681e61e705b920c33b6cc8bec34fcb8cc2531
Heyder Medina <medinaheyder@gmail.com>
2024-07-13 13:26:11 -0600

    fetch computadoras

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('https://api.mercadolibre.com/sites/MLA/search?q=malabares')
-    .then(res => res.json())
-    .then(res => {
-      const productos = res.results;
-      const output = document.getElementById('productos');
-      let htmlString = '';
+  fetch('https://api.mercadolibre.com/sites/MLA/search?q=computadoras')
+      .then(res => res.json())
+      .then(res => {
+          const productos = res.results;
+          const output = document.getElementById('productos');
+          let htmlString = '';
 
-      productos.forEach(producto => {
-        htmlString += `
-                  <div class="card">
-                  
-                    <div class = "prod-img">
-                    <style>
-                      .prod-img{
-                        background-image: url('${producto.thumbnail}');
-                        backgro
-                      }
-                    </style>
-                    </div>
-
-                    <div class = "data">
-                      <span class = "prod-name">${producto.title}</span>
-                      <span class = "prod-precio"> $${producto.price}</span>
-                      <a href="${producto.permalink}" target="_blank">Ver Producto</a>
-                    </div>
-                      
-                  </div>
-              `;
-      });
-
-      output.innerHTML = htmlString;
-    })
-    .catch(err => console.error(err));
+          productos.forEach(producto => {
+              fetch(`https://api.mercadolibre.com/items/${producto.id}`)
+                  .then(res => res.json())
+                  .then(detalleProducto => {
+                      const imagenAltaCalidad = detalleProducto.pictures[0].url;
+                      htmlString += `
+                          <div class="card">
+                              <div class="prod-img">
+                                  <img src="${imagenAltaCalidad}" alt="${producto.title}">
+                              </div>
+                              <div class="data">
+                                  <span class="prod-name">${producto.title}</span>
+                                  <span class="prod-precio"> $${producto.price}</span>
+                                  <a href="${producto.permalink}" target="_blank">Ver Producto</a>
+                              </div>
+                          </div>
+                      `;
+                      output.innerHTML = htmlString;
+                  })
+                  .catch(err => console.error(err));
+          });
+      })
+      .catch(err => console.error(err));
 });
